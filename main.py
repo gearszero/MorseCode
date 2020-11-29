4fdffff011c8ad1f9b09166873ca02c02ad9a39b
gearszero <gearszero0@gmail.com>
2020-11-29 20:24:22 +0100

    Update main.py
    Changing main based on flake8 and adding some comments

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-#Last changes 23.11.2020
-#Morse code coder and decoder
+# Last changes 23.11.2020
+# Morse code coder and decoder
+
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I",
+           "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+           "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1",
+           "2", "3", "4", "5", "6", "7", "8", "9", " "]
+
+morseCode = [".-|", "-...|", "-.-.|", "-..|", "-|", "...-.|",
+             ".--.|", "....|", "..|", ".---|", "-.-|", ".-..|",
+             ".--|", "-.|", "---|", ".--.|", "--.-|", ".-.|",
+             "...|", "-|", "..-|", "...-|", ".--|", "-..-|",
+             "-.--|", "--..|", "-----|", ".----|", "..---|",
+             "...--|", "....-|", ".....|", "-....|", "--...|",
+             "---..|", "----.|", ""]
 
 
-letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-           "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " "]
+# Checking if input is morse code or not
+def is_code(letter):
 
-morseCode = [".-|", "-...|", "-.-.|", "-..|", "-|", "...-.|", ".--.|", "....|", "..|", ".---|", "-.-|", ".-..|", ".--|",
-             "-.|", "---|", ".--.|", "--.-|", ".-.|", "...|", "-|", "..-|", "...-|", ".--|", "-..-|", "-.--|", "--..|",
-             "-----|", ".----|", "..---|", "...--|", "....-|", ".....|", "-....|", "--...|", "---..|", "----.|", ""]
-
-
-#Checking if input is morse code or not
-def isCode(letter):
     if letter == "." or letter == "-":
         return True
+
     else:
         return False
 
-#Converting input in letters to morse code
-def convertToCode(letter):
+
+# Converting input in letters to morse code
+def convert_to_code(letter):
 
     global letters
 
     if letter not in letters:
         return -1
+
     i = letters.index(letter)
     return morseCode[i]
 
-#Converting morse code input to letters
-def convertToLetter(code):
+
+# Converting morse code input to letters
+def convert_to_letter(code):
 
     global morseCode
 
@@ -39,26 +49,29 @@ def convertToLetter(code):
         tmp[x] += "|"
         if tmp[x] not in morseCode:
             return -1
+
         i = morseCode.index(tmp[x])
         tmp[x] = letters[i]
 
     return tmp
 
-#Unit testing
-def unitTest():
+
+# Unit testing
+def unit_test():
+
     checks = 0
     stringCheck = "sos"
     morseCodeCheck = "...|---|...|"
     checkMorse = []
 
-    morseCodeCheckFinal = convertToLetter(morseCodeCheck)
+    morseCodeCheckFinal = convert_to_letter(morseCodeCheck)
     morseCodeCheckFinal = ''.join(morseCodeCheckFinal)
 
     if stringCheck.upper() == morseCodeCheckFinal:
         checks += 1
 
     for i in range(len(stringCheck)):
-        checkMorse += convertToCode(stringCheck[i].upper())
+        checkMorse += convert_to_code(stringCheck[i].upper())
 
     checkMorse = ''.join(checkMorse)
 
@@ -71,27 +84,32 @@ def unitTest():
         return False
 
 
-if unitTest():
+# Checking if unit test return true
+if unit_test():
 
     code = input("Enter morse code or sentence: ")
 
-    if isCode(code[0]):
-        final = convertToLetter(code)
+    # Checking what is first char in input, if it is . or - it will use convert to letter function
+    if is_code(code[0]):
+        final = convert_to_letter(code)
         if final == -1 or final == '':
-            print("ERROR INVALID INPUT, PLEASE USE THIS SYNTAX exp: ...|---|...| for sos")
+            print("ERROR INVALID INPUT, PLEASE USE THIS SYNTAX "
+                  "exp: ...|---|...| for sos")
         else:
             final = ''.join(final)
             print(final)
-
+    # Else it will use convert to code function
     else:
         finalLetter = []
         for i in range(len(code)):
 
-            tmp = convertToCode(code[i].upper())
+            tmp = convert_to_code(code[i].upper())
 
             if tmp == -1:
-                print("ERROR INVALID INPUT, PLEASE USE THIS SYNTAX exp: ...|---|...| for sos")
+                print("ERROR INVALID INPUT, PLEASE USE THIS SYNTAX "
+                      "exp: ...|---|...| for sos")
                 break
+
             else:
                 finalLetter += tmp
 
